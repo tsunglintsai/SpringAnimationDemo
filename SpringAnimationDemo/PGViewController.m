dae29cbce70740f9fb2fa1a8cbe366c0fb991f8b
Henry Tsai <tsunglin.tsai@gmail.com>
2014-05-11 09:17:49 -0700

    Added demo code

--- SpringAnimationDemo/PGViewController.m
+++ SpringAnimationDemo/PGViewController.m
@@ -9,7 +9,12 @@
 #import "PGViewController.h"
 
 @interface PGViewController ()
-
+@property (weak, nonatomic) IBOutlet UIView *viewToAnimate;
+@property (nonatomic) CGFloat dampingRatio;
+@property (nonatomic) CGFloat velocity;
+@property (weak, nonatomic) IBOutlet UILabel *dampingRatioLabel;
+@property (weak, nonatomic) IBOutlet UILabel *velocityLabel;
+@property (nonatomic) CGRect initPosition;
 @end
 
 @implementation PGViewController
@@ -17,13 +22,39 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.dampingRatio = 0.5;
+    self.velocity = 0.5;
+    self.initPosition = self.viewToAnimate.frame;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [self animate];
+
+}
+
+- (IBAction)dampingRatioChagne:(UISlider*)sender {
+    self.dampingRatio = sender.value;
+    self.dampingRatioLabel.text = [@(sender.value) description];
+}
+- (IBAction)velocityLabelChange:(UISlider*)sender {
+    self.velocity = sender.value;
+    self.velocityLabel.text = [@(sender.value) description];
+}
+
+- (IBAction)goButtonTapped:(id)sender {
+    [self animate];
+}
+
+
+- (void)animate{
+    self.viewToAnimate.frame = self.initPosition;
+	// Do any additional setup after loading the view, typically from a nib.
+    [UIView animateWithDuration:1.5 delay:0 usingSpringWithDamping:self.dampingRatio initialSpringVelocity:self.velocity options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        self.viewToAnimate.frame = CGRectMake(self.view.frame.size.width/2, 300, 60, 60);
+    } completion:^(BOOL finished) {
+        self.viewToAnimate.frame = self.initPosition;
+    }];
 }
 
 @end
